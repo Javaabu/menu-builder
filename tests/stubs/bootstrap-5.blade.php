b34adcfad87bd54e9cc9054f9655680e3ef4b449
Arushad Ahmed <dash-8x@hotmail.com>
2024-03-08 00:40:17 +0500

    - Created stubs for menu testing

---
+++ tests/stubs/bootstrap-5.blade.php
@@ -0,0 +1,36 @@
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a href="http://localhost" class="nav-link d-flex justify-content-between align-items-center active">
+            <span><i class="zmdi zmdi-view-quilt"></i> Dashboard</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="" class="nav-link d-flex justify-content-between align-items-center">
+            <span><i class="zmdi zmdi-shopping-cart"></i> Products</span>
+            <span class="ms-2 badge text-bg-primary rounded-pill">21</span>
+        </a>
+        <ul>
+            <li class="nav-item">
+                <a href="http://localhost/products" class="nav-link d-flex justify-content-between align-items-center">
+                    <span>Products</span>
+                    <span class="ms-2 badge text-bg-primary rounded-pill">21</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="http://localhost/categories" class="nav-link d-flex justify-content-between align-items-center">
+                    <span>Categories</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="http://localhost/brands" class="nav-link d-flex justify-content-between align-items-center">
+                    <span>Brands</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-item">
+        <a href="http://localhost/returns" class="nav-link d-flex justify-content-between align-items-center">
+            <span><i class="zmdi zmdi-assignment-return"></i> Returns</span>
+        </a>
+    </li>
+</ul>
---
+++ tests/stubs/material-admin-26.blade.php
@@ -0,0 +1,36 @@
+<ul class="navigation">
+    <li class="navigation__active">
+        <a href="http://localhost">
+            <i class="zmdi zmdi-view-quilt"></i> Dashboard
+        </a>
+    </li>
+    <li class="navigation__sub">
+        <a href="">
+            <i class="zmdi zmdi-shopping-cart"></i> Products
+            <span class="ml-2 badge badge-pill">21</span>
+        </a>
+        <ul>
+            <li>
+                <a href="http://localhost/products" class="d-flex justify-content-between align-items-center">
+                    <span>Products</span>
+                    <span class="badge badge-pill bg-primary align-self-start">21</span>
+                </a>
+            </li>
+            <li>
+                <a href="http://localhost/categories" class="d-flex justify-content-between align-items-center">
+                    <span>Categories</span>
+                </a>
+            </li>
+            <li>
+                <a href="http://localhost/brands" class="d-flex justify-content-between align-items-center">
+                    <span>Brands</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="http://localhost/returns">
+            <i class="zmdi zmdi-assignment-return"></i> Returns
+        </a>
+    </li>
+</ul>
